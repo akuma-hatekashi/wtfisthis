e2932a9ac455136e5fadda0c5f97dca9495a768c
akuma hatekashi <83667192+akuma-hatekashi@users.noreply.github.com>
2021-05-28 12:57:44 -0400

    Create argumentos.sh

---
+++ argumentos.sh
@@ -0,0 +1,8 @@
+# Uso de argumentos
+
+project=$1
+company=$2
+
+echo "El proyecto a abrir es $project de la empresa $company"
+echo "El número de argumentos enviados es: $#"
+echo "Los parametros enviados son :$* " 

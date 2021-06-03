0ab5d2c5adbffd92e1df3746aeba8505ab82ad9d
akuma hatekashi <83667192+akuma-hatekashi@users.noreply.github.com>
2021-06-03 12:05:52 -0400

    Create var,const,liter.c

---
+++ var,const,liter.c
@@ -0,0 +1,10 @@
+int payerScore = 95;
+char ch = 'a';
+// some code
+ch = '1';
+int number = 5;      // integer variable
+number = 5.5;        // error
+double number;       // error
+const double PI = 3.14;
+const double PI = 3.14;
+PI = 2.9; //Error
